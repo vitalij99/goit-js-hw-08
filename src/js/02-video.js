fbb36aa327be22ac9fac6a0a52f43a1e857ccda1
vitalij99 <vitalisklymko@gmail.com>
2023-01-14 21:42:10 +0200

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,18 +3,17 @@ import { throttle } from 'lodash';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-console.log('допоможіть розібратись з з методом catch, і чи потрібен він тут взагалі? помилки наче нема в плеері? хочеться для розібратись, але не розумію цей метод(((');
 
-player.on(
-  'timeupdate',
-    throttle(e => {
-      localStorage.setItem('videoplayer-current-time', e.seconds);
+if (localStorage.getItem('videoplayer-current-time') === null) {
+ localStorage.setItem('videoplayer-current-time', 0)  
+}
+
+player.on('timeupdate', throttle(e => {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
   }, 1000)
-  
 );
 
-player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') )
   .catch(function (error) {
     console.log(error);
   });
