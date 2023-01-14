baf684fd374e37408ccc8d7bee4b8b7e9e196789
vitalij99 <vitalisklymko@gmail.com>
2023-01-14 19:37:44 +0200

    add simple

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,7 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
